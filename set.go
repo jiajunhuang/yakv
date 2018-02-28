644efafc3bc72f267823e186fd5960cf9e70e297
Jiajun Huang <gansteed@gmail.com>
2018-02-28 19:09:04 +0800

    new set op: count, ismember, remove

--- set.go
+++ set.go
@@ -21,3 +21,41 @@ func (s *Set) Add(keys ...[]byte) int {
 
 	return count
 }
+
+// IsMember implements SISMEMBER
+func (s *Set) IsMember(key []byte) bool {
+	_, ok := s.data.Load(b2s(key))
+	return ok
+}
+
+// Remove return numbers removed
+func (s *Set) Remove(keys ...[]byte) int {
+	count := 0
+
+	// FIXME: as we're using sync.Map, it does not provide such operation(delete and count)
+	// so here, the operation `Remove` is not concurrency safe
+	// TODO: maybe change this implementation in sync.Map.Range
+	for i := range keys {
+		key := b2s(keys[i])
+		_, ok := s.data.Load(key)
+		if ok {
+			count++
+		}
+		s.data.Delete(key)
+	}
+
+	return count
+}
+
+// Count return count of elements in sync.Map
+func (s *Set) Count() int {
+	length := 0
+
+	s.data.Range(func(k, v interface{}) bool {
+		length++
+
+		return true
+	})
+
+	return length
+}
--- set_test.go
+++ set_test.go
@@ -6,18 +6,87 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	key1 = []byte("hello")
+	key2 = []byte("world")
+)
+
 // add
 func TestSetAdd(t *testing.T) {
 	s := Set{}
 
-	assert.Equalf(t, 1, s.Add([]byte("hello")), "should euqal")
-	assert.Equalf(t, 0, s.Add([]byte("hello")), "should euqal")
+	assert.Equalf(t, 1, s.Add(key1), "should euqal")
+	assert.Equalf(t, 0, s.Add(key1), "should euqal")
 }
 
 func BenchmarkSetAdd(b *testing.B) {
 	s := Set{}
 
 	for i := 0; i < b.N; i++ {
-		s.Add([]byte("hello"))
+		s.Add(key1)
+	}
+}
+
+// ismember
+func TestSetIsMember(t *testing.T) {
+	s := Set{}
+
+	s.Add(key1)
+
+	assert.Equal(t, true, s.IsMember(key1), "should exist")
+	assert.Equal(t, false, s.IsMember(key2), "should not exist")
+}
+
+func BenchmarkSetIsMember(b *testing.B) {
+	s := Set{}
+
+	for i := 0; i < b.N; i++ {
+		s.IsMember(key1)
+	}
+}
+
+// remove
+func TestSetRemove(t *testing.T) {
+	s := Set{}
+
+	s.Add(key1)
+	assert.Equal(t, true, s.IsMember(key1), "should exist")
+
+	s.Remove(key1)
+	assert.Equal(t, false, s.IsMember(key1), "should not exist")
+}
+
+func BenchmarkSetRemove(b *testing.B) {
+	s := Set{}
+	s.Add(key1)
+
+	for i := 0; i < b.N; i++ {
+		s.Remove(key1)
+	}
+}
+
+// count
+func TestSetCount(t *testing.T) {
+	s := Set{}
+
+	assert.Equal(t, 0, s.Count(), "should equal")
+
+	s.Add(key1)
+	assert.Equal(t, 1, s.Count(), "should equal")
+
+	s.Add(key1)
+	assert.Equal(t, 1, s.Count(), "should equal")
+
+	s.Add(key2)
+	assert.Equal(t, 2, s.Count(), "should equal")
+}
+
+func BenchmarkSetCount(b *testing.B) {
+	s := Set{}
+	s.Add(key1)
+	s.Add(key2)
+
+	for i := 0; i < b.N; i++ {
+		s.Count()
 	}
 }
